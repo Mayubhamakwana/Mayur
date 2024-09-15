bb6d564d56a5df258e734522090630a918be066c
Mayubhamakwana <mayubha.makwana132@gmail.com>
2024-09-15 11:04:58 +0530

    function add.cpp

---
+++ function add.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int add(int c,int d)
+ {
+ 	 return a+b;
+ 
+ 	
+ }
+   int main()
+   {
+   	 int c,d;
+   	 cin>>c>>d;
+   	 add(c,d);
+   	 cout<<"c and d:"<<c+d<<endl;
+   }
+int sub(int c,int d)
+ {
+ 	 return c-d;
+ 
+ 	
+ }
+   int main()
+   {
+   	 int c,d;
+   	 cin>>c>>d;
+   	 sub(c,d);
+   	 cout<<"c and d:"<<c-d<<endl;
+   
+   
+   	 return 0;
+   }
